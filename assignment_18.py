44c3944fe7b546194084fcb0ba1552e7c1a2e94e
monika sharma <monika47456@gmail.com>
2023-06-29 08:29:12 +0530

    Merge remote-tracking branch 'origin/master'

--- assignment_18.py
+++ assignment_18.py
@@ -73,8 +73,86 @@ class Solution:
 
 
 
+# ......................................................ans 5....................................................
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        # Create an empty hash set
+        seen = set()
+        # Iterate over the array
+        for num in nums:
+            # If an element is already in the hash set, return True
+            if num in seen:
+                return True
+            # Otherwise, add the element to the hash set
+            seen.add(num)
+        # If we finish iterating over the array and have not found a duplicate, return False
+        return False
+
+
+
+
+# ...........................................................ans 6...............................................
+    def findMinArrowShots(self, points: list[list[int]]) -> int:
+        # Sort the balloons by their end points in ascending order
+        points.sort(key=lambda x: x[1])
+
+        # Initialize the number of arrows needed and the end point of the last arrow
+        arrows = 0
+        last_end = float('-inf')
+
+        # Iterate through the balloons
+        for start, end in points:
+            # If the current balloon starts after the end of the last arrow, shoot a new arrow
+            if start > last_end:
+                arrows += 1
+                last_end = end
+
+        return arrows
+
+
+
+
+
+# ...................................................................ans 7........................................................
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        n = len(nums)
+        tails = [0] * n # initialize tails array with 0
+        size = 0
+        for num in nums:
+            i, j = 0, size
+            while i != j:
+                m = (i + j) // 2
+                if tails[m] < num:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = num
+            size = max(i + 1, size)
+
+        return size
+
+
+
+
+# .........................................................ans 8.......................................................
+    def find132pattern(self, nums: list[int]) -> bool:
+        stack = []
+        s3 = float('-inf')
+
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] < s3:
+                return True
+            while stack and stack[-1] < nums[i]:
+                s3 = stack.pop()
+            stack.append(nums[i])
+
+        return False
+
+
+
 obj = Solution()
 # ans = obj.merge([[1,3],[2,6],[8,10],[15,18]])
 # ans = obj.sortColors([0,2,1,1,0,0,2,1])
-ans = obj.maximumGap([3,6,9,1])
+# ans = obj.maximumGap([3,6,9,1])
+# ans = obj.lengthOfLIS([10,9,2,5,3,7,101,18])
+ans = obj.find132pattern([-1,3,2,0])
 print(ans)
